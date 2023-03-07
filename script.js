8982f2c7eb13c989b25167318b7eb73c87737cc6
layne nigro <laynenigro@gmail.com>
2023-03-06 19:39:57 -0500

    added api data

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const APIkey = "5P6QO7VJRqnquUEJr7susQ==jNfogauqhz6Oe2kU";
-const categorySelection = document.querySelector(".dropdown-menu");
+const APIkey = "5P6QO7VJRqnquUEJr7susQ==jNfogauqhz6Oe2kU"
+const quoteApi = "https://api.api-ninjas.com/v1/quotes"
+const categorySelection = document.querySelector("#category")
+const submitBtnEl = document.querySelector(".generate")
 
-// var category = 'happiness'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
-    headers: { 'X-Api-Key': "5P6QO7VJRqnquUEJr7susQ==jNfogauqhz6Oe2kU"},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+
+submitBtnEl.addEventListener("click", function (event) {
+    console.log("something");
+    // formEl.classList.add("hide");
+    // populatedDataEl.classList.remove("hide");
+    event.preventDefault()
+    // let type = typeEl.options[typeEl.selectedIndex].text;
+    fetchApi();
+})
 
 //Function to call API
 function fetchApi() {
-    fetch('https://api.api-ninjas.com/v1/quotes?category=' +  categorySelection.options[typeEl.selectedIndex].text, {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=' +  categorySelection.options[categorySelection.selectedIndex].text + '&limit=10', {
         headers: {
             'X-Api-Key': '5P6QO7VJRqnquUEJr7susQ==jNfogauqhz6Oe2kU'
         },
@@ -26,21 +24,23 @@ function fetchApi() {
             return response.json()
         }
     }).then(data => {
-        createExercises(data)
+        // createQuotes(data)
+        console.log(data);
     }).catch(error => {
         console.log(error)
     })
 }
 
 
-categorySelection.addEventListener("click", function (event) {
-    category.textContent = "";
-    const category = event.target.textContent;
-});
 
-window.onload = function() {
-    // generateQuote();
-    document.getElementById("button").addEventListener('click', generateQuote);
-    document.getElementById("button").addEventListener('click', choosePic);
-    choosePic();
-}
+// categorySelection.addEventListener("click", function (event) {
+//     category.textContent = "";
+//     const category = event.target.textContent;
+// });
+
+// window.onload = function() {
+//     // generateQuote();
+//     document.getElementById("button").addEventListener('click', generateQuote);
+//     document.getElementById("button").addEventListener('click', choosePic);
+//     choosePic();
+// }
